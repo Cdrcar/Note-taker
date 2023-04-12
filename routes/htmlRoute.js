c1067df1425e81104b158d8c28a62acdd9feda58
Carmela <carmela881@outlook.com>
2023-04-12 21:26:46 +0100

    added constant to apiRoute

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,20 +5,20 @@ const router = require("express").Router();
 
 // Route handler for endpoint '/'
 router.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
 })
 
 
 // Route handler for endpoint '/notes'
 router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
 })
 
 
 // Route handler for all other URLs
 router.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
 })
 
 // Export router
-module.exports = router
+module.exports = router;
